9ef7c7a044c85f890f674afe4f6c0f7b435d583b
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-09-11 18:43:56 +0100

    Primeiro commit

---
+++ Script.js
@@ -0,0 +1,33 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggler = document.querySelector('.theme-toggler');
+
+// Function to set theme based on localStorage value
+function setTheme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-mode-variables');
+        themeToggler.querySelector('.light-mode').classList.remove('active');
+        themeToggler.querySelector('.dark-mode').classList.add('active');
+    } else {
+        document.body.classList.remove('dark-mode-variables');
+        themeToggler.querySelector('.dark-mode').classList.remove('active');
+        themeToggler.querySelector('.light-mode').classList.add('active');
+    }
+}
+
+// SAVE THEME WHEN CHANGE PAGE
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    setTheme(savedTheme);
+}
+
+// TOGGLE THEME
+if (themeToggler) {
+    themeToggler.addEventListener('click', () => {
+        const isDarkMode = document.body.classList.toggle('dark-mode-variables');
+        const newTheme = isDarkMode ? 'dark' : 'light';
+        localStorage.setItem('theme', newTheme);
+        setTheme(newTheme);
+    });
+}
