d88c06644952a98735efce485ff692a044305fd6
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-09-12 12:44:12 +0100

    Segundo

--- Script.js
+++ Script.js
@@ -1,6 +1,4 @@
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
+//Theme dark or light
 const themeToggler = document.querySelector('.theme-toggler');
 
 // Function to set theme based on localStorage value
@@ -31,3 +29,30 @@ if (themeToggler) {
         setTheme(newTheme);
     });
 }
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            //active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
+        }
+    });
+    
+    // Sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+
